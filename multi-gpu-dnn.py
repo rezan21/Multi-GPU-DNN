9af4df90aa8e7078e2b3f5b3299f2dbb2f50a1af
rezan21 <reza.naghshineh@gmail.com>
2019-11-09 00:23:27 +0000

    Enable gpu growth

--- multi-gpu-dnn.py
+++ multi-gpu-dnn.py
@@ -1,1 +1,15 @@
-print('initialise')
+# You must have tensorflow-gpu
+import tensorflow as tf
+
+# Enable GPU Growth for all GPUs
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
