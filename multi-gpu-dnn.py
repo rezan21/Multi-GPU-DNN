f5ea693f78fd7b4fc4cd950c9538c2efef4b0d66
rezan21 <reza.naghshineh@gmail.com>
2019-11-09 00:31:13 +0000

    run model

--- multi-gpu-dnn.py
+++ multi-gpu-dnn.py
@@ -20,4 +20,39 @@ if gpus:
 # Use TF MirroredStrategy API to distribute training across multiple GPUs
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    pass
+    # Run your model inside
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train/255.0
+    x_test = x_test/255.0
+
+    print(x_train.shape)
+    print(x_train[0].shape)
+
+    model = Sequential()
+
+    model.add(LSTM(128, input_shape=(
+        x_train.shape[1:]),  return_sequences=True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(128))
+    model.add(Dropout(0.1))
+
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(10, activation='softmax'))
+
+    opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
+
+    # Compile model
+    model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer=opt,
+        metrics=['accuracy'],
+    )
+
+    model.fit(x_train,
+              y_train,
+              epochs=3,
+              validation_data=(x_test, y_test))
